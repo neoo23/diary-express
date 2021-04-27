510cc789a7420f5c392ab5415dab2831a5e7ba8c
steffen schlosshauer <you@example.com>
2021-04-27 09:05:54 +0200

    products rest

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+import Express from "express";
+import Products from "./products.js";
+
+// https://www.youtube.com/watch?v=JlgKybraoy4
+
+const app = Express();
+const port = 3000;
+
+app.get("/products", (req, res) => {
+    res.send(Products);
+});
+
+app.get("/products/:id", (req, res) => {
+    // res.send(Products);
+    res.json(Products.find((p) => {
+        return +req.params.id === p.id;
+    }))
+});
+
+app.listen(port, () => console.log("listen on port" + port));
