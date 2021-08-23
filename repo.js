4b0d900e6ab381ae5e053ea37e7a69c39fa2cec4
steffen schlosshauer <you@example.com>
2021-08-23 18:49:07 +0200

    sorted month processing/parsing by recursive call to month2repo(months)

--- repo.js
+++ repo.js
@@ -19,10 +19,12 @@ const days = [];
 
 /**
  * convert month (2016-12.xml) to list of pa objects and append it to pas[]
+ * where month is last element in months (pop()!)#
  *  
  * @param {*} month 
  */
-export const month2repo = function (month) {
+export const month2repo = function (months) {
+    var month = months.pop(); // removes last element from months array
     fs.readFile(config.pas.diaryFolder + month, (err, data) => {
         if (err) {
             console.log("month " + month + " not found. " + err);
@@ -63,6 +65,11 @@ export const month2repo = function (month) {
                 days.push(day_);
             });
             console.log("loading " + month + " into repo ... pas: +" + (paid-1) + "/" + pas.length + ", days: +" + (dayid-1) + "/" + days.length);
+            if(months.length > 0) {
+                // recursive call !!!
+                month2repo(months);
+                // recursive call !!!
+            }
         });
     });
 }
@@ -71,17 +78,18 @@ export const initRepo = function(startYYYY, endYYYY) {
     console.log("loading " + config.pas.repoStartYear + " - " + config.pas.repoEndYear);
     var startYYYY_ =  startYYYY == undefined ? config.pas.repoStartYear : startYYYY;
     var endYYYY_ =  endYYYY == undefined ? config.pas.repoEndYear : endYYYY;
+    var months = [];
     for(var yyyy=startYYYY_; yyyy <= endYYYY_; yyyy++) {
         for(var mm=1; mm<=12; mm++) {
             var month = yyyy + "-" + (mm < 10 ? "0" : "") + mm + ".xml"; // yyyy-mm.xml
             console.log("load " + month);
-            month2repo(month);
-            // PROBLEM: unsorted results because, asyn processing of fs.readFile(...)
-            // TODO: convert to recursion
-            // so month2repo call month2repo again for next month until all months processed, so no sorting neccessary!
-            // here: build month[] than call month2repo(months[]), process first element, recurse with rest until empty. 
+            months.push(month);
+            // PROBLEM: 23.8.2021, unsorted results because, asyn processing of fs.readFile(...)
+            // SOLUTION: build months[] than call month2repo(months[]), process pop() element, recurse with rest (months[]) until empty. 
         }
     }
+    months.reverse(); // ... 2021-12.xml,2021-11.xml,2021-10.xml,2021-09.xml,2021-08.xml, ...
+    month2repo(months); // recursive method!
 }
 
 export const filterPas = function(yyyy, mm, dd, tag) {
